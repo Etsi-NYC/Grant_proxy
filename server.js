3f40b66c14dd0f9c4487fa79bcb74556e842f2b6
Grant Diamond <grantadiamond@gmail.com>
2018-11-03 10:19:38 -0400

    changed start script

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const axios = require('axios');
-const parser = require("body-parser");
+const parser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 4500;
 
@@ -13,63 +13,68 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.all('/*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   next();
- });
+});
 
 //GRANT:
 
 app.get('/not-items', (req, res) => {
-  axios.get(`http://3.16.22.69/not-items`)
-  .then((results) => {
-    res.send(results.data)
-  })
-  .catch((err) => {
-    console.error(err)
-  });
+  axios
+    .get(`http://3.16.22.69/not-items`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 });
 
 app.get('/product/:item', (req, res) => {
-  axios.get(`http://3.16.22.69/product/:item`)
-  .then((results) => {
-    res.send(results.data)
-  })
-  .catch((err) => {
-    console.error(err)
-  });
+  axios
+    .get(`http://3.16.22.69/product/:item`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 });
 
 //KAI:
 
-app.get("/listing/:id", (req, res) => {
-  axios.get(`http://18.191.123.82/listing/:id`)
-  .then((results) => {
-    res.send(results.data)
-  })
-  .catch((err) => {
-    console.error(err)
-  });
+app.get('/listing/:id', (req, res) => {
+  axios
+    .get(`http://18.191.123.82/listing/:id`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 });
 
-app.get("/api/:id", (req, res) => {
-  axios.get(`http://18.191.123.82/api/:id`)
-  .then((results) => {
-    res.send(results.data)
-  })
-  .catch((err) => {
-    console.error(err)
-  });
+app.get('/api/:id', (req, res) => {
+  axios
+    .get(`http://18.191.123.82/api/:id`)
+    .then(results => {
+      res.sendFile(path.join(__dirname, './public/index.html'));
+    })
+    .catch(err => {
+      console.error(err);
+    });
 });
 
 //WYATT:
 
 app.get('/reviews', (req, res) => {
-  axios.get(`http://ec2-18-191-75-80.us-east-2.compute.amazonaws.com/reviews`)
-  .then((results) => {
-    res.send(results.data)
-  })
-  .catch((err) => {
-    console.error(err)
-  });
-})
+  axios
+    .get(`http://ec2-18-191-75-80.us-east-2.compute.amazonaws.com/reviews`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
 
 // -------------------
 
